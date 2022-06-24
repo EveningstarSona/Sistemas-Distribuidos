eb779e705d17e210f80bcd99e0ecdce33399875e
EveningstarSona <iamsonaeveningstar@gmail.com>
2022-06-24 09:57:21 -0300

    Finished chatroom at Exercise 1

--- Exercicio1/cliente.py
+++ Exercicio1/cliente.py
@@ -1,15 +1,33 @@
 import socket
+import threading
 
 HOSTNAME = '127.0.0.1'
 PORT = 777
 
+def recv(s: socket.socket):
+    while True:
+        try:
+            data = s.recv(4096)
+            print(data.decode())
+        except:
+            print('An error ocurred.')
+            s.close()
+            break
+
+def send(s: socket.socket, recv_thread: threading.Thread):
+    while recv_thread.is_alive():
+        message = input()
+        s.send(message.encode())
+
 def main():
     s = socket.socket()
     s.connect((HOSTNAME, PORT))
-    s.sendall(b'Estou vivo!')
-    while (data := s.recv(4096)):
-        s.sendall(input().encode())
-        print(data.decode())
+    recv_thread = threading.Thread(target=recv, args=(s,))
+    recv_thread.start()
+    send_thread = threading.Thread(target=send, args=(s,recv_thread))
+    send_thread.start()
+    recv_thread.join()
+
     s.close()
 
 if __name__ == '__main__':
--- Exercicio1/servidor.py
+++ Exercicio1/servidor.py
@@ -5,16 +5,34 @@ from typing import Any
 HOSTNAME = '127.0.0.1'
 PORT = 777
 
-CLIENTS: dict[Any, socket.socket] = {}
+SocketAddress = tuple[str, int]
+
+CLIENTS: dict[SocketAddress, socket.socket] = {}
+NICKNAMES: dict[SocketAddress, str] = {}
 THREADS: list[threading.Thread] = []
 
-def client_thread(client: socket.socket, addr: Any):
+def set_client_nick(addr: Any, nickname: str):
+    if nickname in NICKNAMES.values():
+        return 'Nickname já escolhido.'
+    old_nick = NICKNAMES[addr]
+    NICKNAMES[addr] = nickname
+    return f'Seu nickname é {nickname}.' if old_nick == '*' else f'{old_nick}, seu novo nickname é {nickname}'
+
+def broadcast(addr: SocketAddress, message: str):
+    for client in CLIENTS.keys():
+        if CLIENTS[client] is not CLIENTS[addr]:
+            CLIENTS[client].sendall(f'{NICKNAMES[addr]}: {message}'.encode())
+
+def client_thread(client: socket.socket, addr: SocketAddress):
     CLIENTS.update({addr: client})
+    NICKNAMES.update({addr: '*'})
     print(client, addr)
-    data = 'data'
-    while data:
-        data = client.recv(4096)
-        print(data.decode())
+    client.sendall(b'Bem-vindo ao servidor! Use /nick [nickname].')
+    while (data := client.recv(4096)) != b'':
+        if data.decode().split()[0] == '/nick':
+            client.sendall(set_client_nick(addr, " ".join(data.decode().split()[1:])).encode())
+        else:
+            broadcast(addr, data.decode())
     client.close()
 
 def main():
